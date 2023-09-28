82851d3f9863e3a3f2d6a68ac7a5cc2c233806d9
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-28 11:08:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,52 @@ const apiUrls = [
 ];
 
 // You can write your code here
+ function fetchData(apiUrls) {
+      return fetch(apiUrls).then(response => response.json());
+    }
+
+    // Function to fetch data from multiple APIs using Promise.all
+    function fetchAllData() {
+      const startTime = performance.now();
+      const promises = apiUrls.map(fetchData);
+
+      Promise.all(promises)
+        .then(data => {
+          const endTime = performance.now();
+          const timeTaken = endTime - startTime;
+          displayResult('Promise.all', timeTaken);
+        })
+        .catch(error => {
+          displayResult('Promise.all', 'Error: ' + error.message);
+        });
+    }
+
+    // Function to fetch data from multiple APIs using Promise.any
+    function fetchAnyData() {
+      const startTime = performance.now();
+      const promises = apiUrls.map(fetchData);
+
+      Promise.any(promises)
+        .then(data => {
+          const endTime = performance.now();
+          const timeTaken = endTime - startTime;
+          displayResult1('Promise.any', timeTaken);
+        })
+        .catch(error => {
+          displayResult('Promise.any', 'Error: ' + error.message);
+        });
+    }
+
+    // Function to display the results on the webpage
+    function displayResult(method, timeTaken) {
+      const resultsDiv = document.getElementById('output-all');
+      resultsDiv.innerHTML = `${method} took ${timeTaken.toFixed(1)} milliseconds.`;
+    }
+function displayResult1(method, timeTaken) {
+      const resultsDiv = document.getElementById('output-any');
+      resultsDiv.innerHTML = `${method} took ${timeTaken.toFixed(1)} milliseconds.`;
+    }
+
+    // Call both fetch methods when the page loads
+    fetchAllData();
+    fetchAnyData();
